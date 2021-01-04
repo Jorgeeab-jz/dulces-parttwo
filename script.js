128a3badcb579e2b65a7333f9d623b8cc25303e7
Jorge Abdallah <jorgeea1205@gmail.com>
2021-01-04 17:08:40 -0400

    -Name changes -Added 'Add-to-cart' functionality -Changed cart modal to close on a button

--- script.js
+++ script.js
@@ -22,6 +22,17 @@ const cartBtn = document.querySelector('.shopping-container');
 const shopIcon = document.querySelector('.shopping-container i');
 const cartModal = document.querySelector('.cart-modal');
 const cartItems = document.querySelector('.cart-items');
+let addBtn = document.querySelectorAll('.add');
+
+productsDB.once('value') //Stores the available products in an object
+  .then(function (snap) {
+    products = snap.val();
+    drawProductList();
+    console.log('ready');
+  })
+  .catch(function (err) {
+    console.log('Error', err.code);
+});
 
 function drawProductList() {
     for (const [key,value] of Object.entries(products)) {
@@ -33,7 +44,6 @@ function drawProductList() {
          }
       }
 }
-
 
 function drawCategory(productType) {
     let productList = document.getElementById('product-list');
@@ -47,52 +57,124 @@ function drawCategory(productType) {
 
 function drawProducts(product,category) {
     let productContainer = document.createElement('div');
+    
     productContainer.className = 'product';
+    
+    productContainer.dataset.id = product.id;
+    
     let productName = document.createElement('h3');
+    
     productName.innerText = product.name;
+    
     let productPrice = document.createElement('h4');
-    productPrice.innerText = product.price
+    
+    productPrice.innerText = `${product.price}$`;
+    
     let btnContainer = document.createElement('div');
+    
     btnContainer.className = 'qty-btn'
+    
     let addBtn = document.createElement('button');
-    addBtn.innerText = '+';
-    let lessBtn = document.createElement('button');
-    lessBtn.innerText = '-';
-    btnContainer.append(addBtn,lessBtn);
+    
+    addBtn.innerHTML = '<i class="fas fa-cart-arrow-down"></i>';
+    
+    addBtn.className = 'add';
+
+    addBtn.addEventListener('click', (e) => {
+        let items = document.querySelectorAll('.item');
+        for (let i = 0; i <= items.length; i++) {
+            let product = (e.target).parentElement.parentElement.parentElement;
+            if (items[i] !== undefined) {
+                if (items[i].dataset.id == product.dataset.id) {
+                    alert('This item is already added to the cart')
+                    return;
+                }
+            }else{
+                addCartItem(product);
+            }
+        }
+    })
+    
+    btnContainer.append(addBtn);
+    
     productContainer.append(productName,productPrice,btnContainer);
+    
     category.append(productContainer);
 }
-
-productsDB.once('value') //Stores the available products in an object
-  .then(function (snap) {
-    products = snap.val();
-  })
-  .catch(function (err) {
-    console.log('Error', err.code);
-});
 
 function showModal(modal) {
     modal.style.display = 'block';
 }
+
 function hideModal(modal) {
     modal.style.display = 'none';
 }
+
 function addTotal() {
     let total = qtyDisplay.value * rdc.price;
     totalDisplay.textContent = `${(Math.round(total * 10) / 10)}$`;
 }
 
+function addCartItem(item) {
+    let cartItemContainer = document.createElement('div');
+    
+    cartItemContainer.className = 'item';
+    
+    cartItemContainer.dataset.id = item.dataset.id;
+    
+    let itemName = document.createElement('p');
+    
+    itemName.innerText = item.querySelector('h3').innerText;
+    
+    cartItemContainer.append(itemName);
+
+    let qty = document.createElement('input');
+
+    qty.value = 1;
+    
+    qty.setAttribute('type','number');
+
+    qty.className = 'qty-input';
+
+    qty.addEventListener('change', quantityChanged);
+
+    let removeBtn = document.createElement('button');
+
+    removeBtn.innerText = 'X';
+
+    removeBtn.className = 'remove-btn';
+
+    removeBtn.addEventListener('click', (e)=> {
+        let buttonClicked = e.target.parentElement;
+        buttonClicked.remove();
+    })
+    
+    cartItemContainer.append(qty,itemName,removeBtn);
+
+    cartItems.append(cartItemContainer);
+
+    items = document.querySelectorAll('.item');
+}
+
+function quantityChanged(event) {
+    let input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }else if(input.value > 99) {
+        input.value = 99
+    }
+}
+//////////////////////////////////////////////////////////////////////////////////////////
 cartBtn.addEventListener('click', function(){
     showModal(cartModal);
 })
-document.addEventListener('click', (e) => {
-    if(e.target !== cartModal && e.target !== cartBtn && e.target !== shopIcon){
-        hideModal(cartModal);
-    }
-} )
+
+document.getElementById('close-cart').addEventListener('click', ()=>{
+    hideModal(cartModal)
+});
+
 modalShowBtn.addEventListener('click', function(){
     showModal(productsModal);
-    drawProductList();
 });
 modalClose.addEventListener('click', hideModal);
 productsModal.addEventListener('click',(e)=> {
@@ -100,4 +182,3 @@ productsModal.addEventListener('click',(e)=> {
         hideModal(productsModal);
     }
 })
-
